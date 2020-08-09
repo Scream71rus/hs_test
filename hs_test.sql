492306202b76c40e54bcf5d44354f22d677023d4
dima <scream71rus@mail.ru>
2020-08-09 13:26:21 +0300

    Init

---
+++ hs_test.sql
@@ -0,0 +1,21 @@
+create schema hs_test;
+
+create type hs_test.gender_type as enum(
+    'MALE',
+    'FEMALE'
+);
+
+create table hs_test.patient(
+    id serial primary key,
+
+    first_name text not null,
+    middle_name text,
+    last_name text not null,
+
+    gender hs_test.gender_type not null,
+    birthday timestamp not null,
+    address text,
+    medical_insurance text not null,
+
+    created timestamp not null default now()
+);
